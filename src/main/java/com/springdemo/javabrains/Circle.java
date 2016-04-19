6a9bc2b3243e92acc06ef019f2046079cfde0b7c
henriqueschorr <henrique.schorr@gmail.com>
2016-04-18 21:27:20 -0300

    Latest Spring version + Autowired Annotation

--- src/main/java/com/springdemo/javabrains/Circle.java
+++ src/main/java/com/springdemo/javabrains/Circle.java
@@ -1,19 +1,16 @@
 package com.springdemo.javabrains;
 
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class Circle implements Shape{
 	
+	@Autowired
 	private Point center;
 	
 	public Point getCenter() {
 		return center;
 	}
-
-	@Required
-	public void setCenter(Point center) {
-		this.center = center;
-	}
+	
 
 	public void draw() {
 		System.out.println("Drawing Circle");
